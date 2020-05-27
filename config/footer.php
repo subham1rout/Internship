6d2990fa66f6d7d969e099e0193a8904df560620
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2020-05-27 21:48:09 +0530

    Add files via upload

---
+++ config/footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer class="container-fluid mt-3 py-1 text-center" style="background-color:#90ee90;">Copyright 2020 techhub-internship-spectrum</footer>
+</body>
+</html>
